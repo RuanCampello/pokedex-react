b340bb27aa7de692a0abec92f878b0d5ad83ff30
RuanCampello <ruancampello888@gmail.com>
2023-12-21 18:32:51 -0300

    style: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href='@/app/img/favicon.ico' />
+      </head>
       <body className={firaSans.className}>{children}</body>
     </html>
   )
